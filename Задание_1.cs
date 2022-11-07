c57530fb170d3dbe53a58c1e3d4a61daf4b380d3
user <user@WIN-SS6N9OIOE40>
2022-11-07 17:20:00 +0300

    Добавьте файлы проекта.

---
+++ Задание_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Задача__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = string.Empty;
+            int? min = null;
+            while (true)
+            {
+                Console.WriteLine("Введите число, для окончания ввода дважды нажмите <Enter>:");
+                Console.WriteLine();
+                s = Console.ReadLine();
+                int input;
+                if (s == string.Empty) break;
+                if (!int.TryParse(s, out input))
+                    Console.WriteLine("Число введено неправильно");
+                else
+                    min = (min == null || min.Value > input) ? input : min;
+            }
+            Console.WriteLine($"Минимальное число из введённой последовательности: {min}");
+
+            Console.ReadKey();
+        
+    
+        }
+    }
+}
